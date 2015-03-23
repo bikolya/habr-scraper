f94aee68794d7b3c0ccf63a6dc908eaca008a4e1
Nikolay Bienko <bikolya@gmail.com>
2015-03-23 18:35:35 +0300

    Get all post titles from first page

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-url = ARGV[0]
+url = ARGV[0] || "http://habrahabr.ru/company/zfort/"
 
 data = Nokogiri::HTML(open(url, "User-Agent" => "Mac Safari"))
 
-puts data.css('.post_title').text
+data.css('.post_title').each { |title| puts title.text }
+
