cc470a182f870ebea729414dcf9c99f82c40a4a9
Nikolay Bienko <bikolya@gmail.com>
2015-03-24 03:48:59 +0300

    Apply script for my purposes and refactor it

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
 class Scraper
-
-  attr_accessor :base_url, :filter, :titles, :numbers, :ratings, :views, :stars, :comments
+  attr_accessor :titles, :numbers, :ratings, :views, :stars, :comments
 
   FILTERS = { it:      ["Дайджест", "IT", "айти"],
-              php:     [],
-              webdev:  [],
-              python:  [],
-              bigdata: []
+              php:     ["Дайджест", "PHP", "PHP"],
+              webdev:  ["Несколько", "интересностей", "полезностей"],
+              python:  ["Python", "digest", "Новости"],
+              bigdata: ["Обзор", "анализу", "обучению"]
             }
-
-  def initialize
-  end
 
   def set_data(posts)
     @titles = posts.css('.post_title').map{ |title| title.text }
-    @numbers = @titles.map{ |title| title.gsub(/[\s\-]/, "").match(/[№#]\d*/)[0][1..-1].to_i if title.match(/№\d*/) }.compact
+    @numbers = @titles.map{ |title| title.gsub(/[\s\-]/, "").match(/[№#]\d*/)[0][1..-1].to_i if title.match(/№\d*/) }
     @ratings = posts.css('.score').map{ |score| score.text =~ /\d/ ? score.text.to_i : 0 }
     @views = posts.css('.pageviews').map{ |views| views.text.to_i }
     @stars = posts.css('.favs_count').map{ |stars| stars.text.to_i }
@@ -30,15 +27,22 @@ class Scraper
 
   def filtered(info, filter)
     info.select do |post|
-      post[5] =~ /#{filter[0]}.*#{filter[1]}/ || post[5] =~ /#{filter[0]}.*#{filter[2]}/
+      post[5] =~ /#{filter[0]}.*#{filter[1]}/ || post[5] =~ /#{filter[0]}.*#{filter[2]}/ && post[0]
     end
   end
 
-  def scrape_to_csv(base_url, filter)
+  def write_to_csv(data, dest)
+    CSV.open(dest, "w") do |csv|
+      data.each do |post|
+        csv << post
+      end
+    end
+  end
+
+  def scrape_to_csv(base_url, filter, dest)
     page = 1
-    csv = []
+    result = []
     loop do
-      start = Time.now
       url = base_url + "page#{page}"
       data = Nokogiri::HTML(open(url, "User-Agent" => "Mac Safari"))
       posts = data.css('.post')
@@ -48,27 +52,26 @@ class Scraper
       info = filtered(info, filter)
 
       page += 1
-      csv += info
-      finish = Time.now
-      puts finish - start
+      result += info
       break if data.css('#next_page').empty?
     end
 
-    csv
+    result.sort_by!(&:first)
+    write_to_csv(result, dest)
   end
-
 end
 
 
 start = Time.now
-base_url = "http://habrahabr.ru/company/zfort/"
-scraper = Scraper.new
-csv = scraper.scrape_to_csv(base_url, Scraper::FILTERS[:it])
+base_url = ARGV[0] || "http://habrahabr.ru/company/zfort/"
+filter = (ARGV[1] || "it").to_sym
+dest = ARGV[2] || "it.csv"
 
-csv.each do |post|
-  puts post[0]
-end
-csv.sort_by!(&:first)
+scraper = Scraper.new
+scraper.scrape_to_csv(base_url, Scraper::FILTERS[filter], dest)
+
+finish = Time.now
+puts "Completed in #{finish - start}"
 
 # csv[0] - number of digest
 # csv[1] - raings
@@ -76,10 +79,3 @@ csv.sort_by!(&:first)
 # csv[3] - stars
 # csv[4] - comments
 # csv[5] - title
-
-csv.each do |post|
-  puts post.join(", ")
-end
-
-finish = Time.now
-puts finish - start
